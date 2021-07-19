b9125ba6110ddf664d7471a141dc0c88cba86a60
Ryan O'Connor <ryanroc8@gmail.com>
2021-07-19 17:37:49 +0100

    initial commit

---
+++ IrishHousingEstate.AuthenticationApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IrishHousingEstate.AuthenticationApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ IrishHousingEstate.ModelApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IrishHousingEstate.ModelApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

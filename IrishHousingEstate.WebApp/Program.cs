c58ccb29051d338966a33e9d14f3c22cba412d9d
Ryan O'Connor <ryanroc8@gmail.com>
2021-07-23 16:06:44 +0100

    integrated front-end api and converted it into PWA

--- IrishHousingEstate.WebApp/Program.cs
+++ IrishHousingEstate.WebApp/Program.cs
@@ -1,26 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace IrishHousingEstate.WebApp
+namespace CreativeTim.Argon.DotNetCore.Free
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        static IWebHostBuilder CreateWebHostBuilder(string[] args) => 
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureKestrel(so => {})
+                .UseIISIntegration()
+                .UseStartup<Startup>();
     }
 }
